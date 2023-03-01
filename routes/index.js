db6952e828192a9dedc1abfabe6bc2a3690a1296
pragati chaudhary <pragatichaudhary23@gmail.com>
2023-03-01 16:59:16 +0545

    i have changed in the route model

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,16 @@
-const express = require('express');
-const router = require('router');
+//for route model using the express model
+const express = require("express");
+//for route model using router express
+const router = require("router");
 
+//for home page router
+router.get("/", (req, res) => {
+  res.status(404);
+});
+
+//About the page router
+router.get("/about", (req, res) => {
+  res.send("about the Application page");
+});
+
+module.exports = router;
